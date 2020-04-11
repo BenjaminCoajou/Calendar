f098673d965fa7425894c98a5a4f25963834b557
BenjaminCoajou <coajou.benjamin@gmail.com>
2020-04-11 15:10:38 +0200

    dynamic calendar, only structure

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,74 @@
 // == Import npm
 import React from 'react';
-
+import moment from 'moment';
 // == Import
 import Calendar from '../Calendar';
 import './styles.css';
 
 // == Composant
+
+const table = {
+  days:['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'],
+  month: 'Avril',
+  dates: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
+  year: 2020,
+}
+
+moment.locale('fr');
+
+const weekDayName = (
+  moment.weekdays());
+
+const state = {
+  dateObject: moment(),
+};
+
+// premier jour du mois
+const firstDayOfMonth = () => (
+  moment(state.dateObject).startOf('month').format('d')
+);
+// zone de vide pour le début du mois
+const blanks = [];
+for (let i = 0; i < firstDayOfMonth(); i++) {
+  blanks.push(<td>{""}</td>);
+};
+
+const currentDay = () => (state.dateObject.format('D'));
+// nombre de jours dans le mois actuel
+const daysInMonth = [];
+for(let day = 1; day <= state.dateObject.daysInMonth(); day++){
+  let today = day == currentDay() ? "today" : ""
+  daysInMonth.push(
+  <td key={day} className={today}>{day}</td>
+  );
+};
+
+// 
+const totalSlots = [...blanks, ...daysInMonth];
+let rows = [];
+let cells = [];
+totalSlots.forEach((row, i) => {
+  if(i %7 !== 0) {
+    cells.push(row);
+  } else {
+    rows.push(cells);
+    cells = [];
+    cells.push(row);
+  }
+  if ( i === totalSlots.length -1) {
+    rows.push(cells);
+  }
+});
+
+const allDaysInMonth = rows.map((day) => (
+  <tr>{day}</tr> // possible probleme de key
+))
+
+
 const App = () => (
   <div className="app">
-    <Calendar />
+    {console.log(rows)}
+    <Calendar allDays={allDaysInMonth} days={weekDayName}/>
   </div>
 );
 
--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,76 +1,38 @@
 import React from 'react';
+import Proptypes from 'prop-types';
+
+
 import { IoIosArrowBack } from 'react-icons/io';
 import { IoIosArrowForward } from 'react-icons/io';
 
 import './style.css';
 
-const Calendar = () => (
+const Calendar = ({allDays, days}) => (
     <div>
         <div>
             <span><IoIosArrowBack/></span>
-            <span>Avril</span>
-            <span>2020</span>
+            <span> avril </span>
+            <span> 2020 </span>
             <span><IoIosArrowForward/></span>
         </div>
         <div>
             <table>
                 <thead>
-                    <tr>           
-                        <th> Lundi </th>
-                        <th> Mardi </th>
-                        <th> Mercredi </th>
-                        <th> Jeudi </th>
-                        <th> Vendredi </th>
-                        <th> Samedi </th>
-                        <th> Dimanche </th>
+                    <tr>  
+                        {
+                            days.map(day =>(
+                                <th key={day}> {day} </th>
+                            ))                        
+                        }         
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                        <td>4</td>
-                        <td>5</td>
-                        <td>6</td>
-                        <td>7</td>
-                    </tr>
-                    <tr>
-                        <td>8</td>
-                        <td>9</td>
-                        <td>10</td>
-                        <td>11</td>
-                        <td>12</td>
-                        <td>13</td>
-                        <td>14</td>
-                    </tr>
-                    <tr>
-                        <td>15</td>
-                        <td>16</td>
-                        <td>17</td>
-                        <td>18</td>
-                        <td>19</td>
-                        <td>20</td>
-                        <td>21</td>
-                    </tr>
-                    <tr>
-                        <td>22</td>
-                        <td>23</td>
-                        <td>24</td>
-                        <td>25</td>
-                        <td>26</td>
-                        <td>27</td>
-                        <td>28</td>
-                    </tr>
-                    <tr>
-                        <td>29</td>
-                        <td>30</td>
-                        <td>31</td>
-                    </tr>
+                    {allDays}
                 </tbody>
             </table>
         </div>
     </div>
 );
+
 
 export default Calendar;
