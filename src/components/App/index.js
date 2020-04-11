a5d76c694d2b371258cd0e2c4588f38eeef5ff7f
BenjaminCoajou <coajou.benjamin@gmail.com>
2020-04-11 11:45:46 +0200

    structure calendar

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import Calendar from '../Calendar';
+import './styles.css';
+
+// == Composant
+const App = () => (
+  <div className="app">
+    <Calendar />
+  </div>
+);
+
+// == Export
+export default App;
